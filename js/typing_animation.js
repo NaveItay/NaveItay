cce7fa1fc1586c93ddfb9d1b586dfae65709d819
naveitayx <itay.n@phytech.com>
2023-11-14 19:38:24 +0200

    Update typing_animation.js

--- js/typing_animation.js
+++ js/typing_animation.js
@@ -1,6 +1,6 @@
 // Typing animation
 const typed = new Typed(".typing-text", {
-    strings: ["Comuper Vision", "Robotics"],
+    strings: ["Embedded Engineer", "Firmware Engineer"],
     typeSpeed: 150,
     backSpeed: 150,
     loop: true
