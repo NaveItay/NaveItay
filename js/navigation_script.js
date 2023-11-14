9e0ae28bdf640a5f8229d2cf7bf472ebbcfa9f80
naveitayx <itay.n@phytech.com>
2023-11-14 20:55:13 +0200

    update navigation_script

--- js/navigation_script.js
+++ js/navigation_script.js
@@ -16,11 +16,8 @@ navBtns.forEach(navBtn => {
     }
 });
 
-
-
 /*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
-const sections = document.querySelectorAll('section[id]')
-
+const sections = document.querySelectorAll('section[id]');
 
 function scrollActive() {
     const scrollY = window.scrollY;
@@ -29,22 +26,24 @@ function scrollActive() {
     sections.forEach(current => {
         const sectionHeight = current.offsetHeight;
         const sectionTop = current.offsetTop - 70;
+        sectionId = current.getAttribute('id');
 
         if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu-btn[href*=' + sectionId + ']').classList.add('active-blue'); // Add the active class
             scrollTopElement.style.display = 'block'; // Show the scroll-top element
         } else {
+            document.querySelector('.nav-menu-btn[href*=' + sectionId + ']').classList.remove('active-blue'); // Remove the active class
             scrollTopElement.style.display = 'none'; // Hide the scroll-top element
-        };
+        }
     });
 };
-
 
 window.addEventListener('scroll', scrollActive)
 
 // Scroll button
 function scrollTop() {
     let scrollTop = document.getElementById('scroll-top');
-    // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+    // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
     if (this.scrollY >= 200) 
         scrollTop.classList.add('show-scroll'); 
     else 
