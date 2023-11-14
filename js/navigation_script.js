18ed9614f24a287f8ed2e5d270fd3b8232f7a39b
naveitayx <itay.n@phytech.com>
2023-11-14 21:18:04 +0200

    Update navigation_script.js

--- js/navigation_script.js
+++ js/navigation_script.js
@@ -44,7 +44,7 @@ window.addEventListener('scroll', scrollActive)
 function scrollTop() {
     let scrollTop = document.getElementById('scroll-top');
     // When the scroll is higher than 200 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if (this.scrollY >= 200) 
+    if (window.scrollY >= 200) 
         scrollTop.classList.add('show-scroll'); 
     else 
         scrollTop.classList.remove('show-scroll');
